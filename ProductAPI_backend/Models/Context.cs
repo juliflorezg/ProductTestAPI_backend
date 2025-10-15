18725aa01eac57c9afb2de7cbf49373d7b43d299
Julian Florez <forezjuli08@hotmail.com>
2025-10-15 14:45:56 -0500

    feat(ProductAPI): add db context for ef core

---
+++ ProductAPI_backend/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductAPI_backend.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
