dfac5e96ab73b2d52b33f1c5800094e2f8636cc4
Julian Florez <forezjuli08@hotmail.com>
2025-10-15 14:23:19 -0500

    Add project files.

---
+++ ProductAPI_backend/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductTestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult GetProductById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
