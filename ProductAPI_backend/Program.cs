6e8b2f3f80e64471f56f72ad8649c9c82232a97d
Julian Florez <forezjuli08@hotmail.com>
2025-10-15 21:07:19 -0500

    feat(ProductAPI): add services to builder

--- ProductAPI_backend/Program.cs
+++ ProductAPI_backend/Program.cs
@@ -1,11 +1,37 @@
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ProductAPI_backend.DTOs;
+using ProductAPI_backend.Models;
+using ProductAPI_backend.Repository;
+using ProductAPI_backend.Services;
+using ProductAPI_backend.Validators;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IRepository<Product, Guid>, ProductsRepository>();
+builder.Services.AddScoped<ICommonService<ProductDTO, ProductInsertDTO, ProductUpdateDTO, Guid>, ProductService>();
+builder.Services.AddScoped<IValidator<ProductInsertDTO>, ProductInsertDtoValidator>();
+builder.Services.AddScoped<IValidator<ProductUpdateDTO>, ProductUpdateDtoValidator>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -17,6 +43,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
