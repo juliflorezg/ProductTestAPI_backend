e491dc329399d7a6abeca37bc51f485aac8e0eb3
Julian Florez <forezjuli08@hotmail.com>
2025-10-15 21:05:22 -0500

    feat(ProductAPI): add DTOs

---
+++ ProductAPI_backend/DTOs/ProductUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductAPI_backend.DTOs
+{
+    public class ProductUpdateDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
